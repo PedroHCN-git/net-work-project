8cdd072583eb89cc62002e673ef4dff3a1580b9e
PedroHCN <pedro.hcorradini05@gmail.com>
2025-06-19 21:20:33 -0300

    register endpoint and html for register

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ def login():
     elif request.method == 'POST':
         user_name_email = request.form.get('userNameOrEmail', '')
         password = request.form.get('password', '')
-        user_auth()
+        user_auth(user_name_email, password)
 
 
-def user_auth():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        user_name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        register_user()
+
+
+def user_auth(user_name_email: str, user_password: str):
     ...
 
 
-def find_user():
+def register_user(user_name: str, email: str, user_password: str):
     ...
